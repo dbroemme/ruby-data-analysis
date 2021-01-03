755674e76e3bd575518848e6dfbea78e34c36983
dbroemme <darren.broemmer@gmail.com>
2021-01-03 13:24:39 -0500

    Minor change

--- analyze_lottery.rb
+++ analyze_lottery.rb
@@ -1,6 +1,8 @@
 require_relative 'lib/helpers'
 require_relative 'lib/counter'
 
+TOTAL_COUNT = "total"
+
 # Step 1: Read the data file
 # Step 2: For each line, split up the data into its parts
 # Step 3: Keep track of the number of occurrences for each number
@@ -18,26 +20,39 @@ require_relative 'lib/counter'
 # 9/30/1991; Night: 3,3,4,5
 #
 # All information is entered manually, and is subject to human error.
-# Therefore, we can not guarantee the accuracy of this information.
+# Therefore, we can not guarantee the accuracy of this information.c
+
+def display_row(n, c, p)
+    puts "#{pad(n, 10)}  #{pad(c, 10)}  #{pad(p, 10)}"
+end
 
 DATA_FILE = "./data/Pick4_12_21_2020.txt"
 counter = Counter.new("Lottery Numbers")
+weekday_counters = []
+(0..6).each do |day_of_week|
+    weekday_counters[day_of_week] = Counter.new(Date::DAYNAMES[day_of_week])
+end
+weekday_counter = nil 
 
 puts "Read the data file #{DATA_FILE}"
 File.readlines(DATA_FILE).each do |line|
     line = line.chomp  # remove the carriage return
 
     if line.length > 0 and line[0].match(/[0-9]/)
-        # A data line looks like this
+        # A line from the file with data looks like this
         # 12/20/2020; Day: 7,1,5,9; Night: 3,5,9,2
         sections = line.split(";")
         # The sections are [0] date
         #                  [1] drawing1
         #                  [2] drawing2
+        date = Date.strptime(sections[0], "%m/%d/%Y")
+        weekday_counter = weekday_counters[date.wday]
         # There could be either two or three sections
-        # as older lottery drawings only occurred at night
+        # (one or two drawings) because older lottery
+        # drawings only occurred at night
         (1..sections.size-1).each do |section_index|
-            counter.increment("total")
+            counter.increment(TOTAL_COUNT)
+            weekday_counter.increment(TOTAL_COUNT)
             section = sections[section_index]
             # A section has the format:  Day: 7,1,5,9
             # Extract just the number portion and then
@@ -46,21 +61,30 @@ File.readlines(DATA_FILE).each do |line|
             numbers = str_numbers.split(",")
             numbers.each do |str_number|
                 counter.increment(str_number.to_i)
+                weekday_counter.increment(str_number.to_i)
             end
         end
     end
 end
 
-total = counter.count("total")
-newline
-puts "#{pad('Number', 10)}  #{pad('Count', 10)}  #{pad('Percent', 10)}"
-puts "#{'-' * 10}  #{'-' * 10}  #{'-' * 10}"
-counter.get_sorted_array.each do |number_and_count_array|
-    # The sorted array elements are [number, count]
-    number = number_and_count_array[0]
-    occurrences = number_and_count_array[1]
-    percent = (occurrences.to_f / total.to_f).round(5)
-    puts "#{pad(number,10)}  #{pad(occurrences,10)}  #{pad(percent,10)}"
+def display_counter(counter, title)
+    total = counter.count(TOTAL_COUNT)
+    puts '*' * 34
+    puts title
+    newline
+    display_row("Number", "Count", "Percent")
+    display_row('-' * 10, '-' * 10, '-' * 10)
+    counter.get_sorted_array.each do |number_and_count_array|
+        # The sorted array elements are [number, count]
+        number = number_and_count_array[0]
+        occurrences = number_and_count_array[1]
+        percent = (occurrences.to_f / total.to_f).round(5)
+        display_row(number, occurrences, percent)
+    end
+    newline
 end
-newline
 
+display_counter(counter, "All Days")
+(0..6).each do |day_of_week|
+    display_counter(weekday_counters[day_of_week], Date::DAYNAMES[day_of_week])
+end
